e3d9d95778b2be4384091cce14cd3caf4394c44f
MadLord <madlord@list.ru>
2020-01-28 17:40:06 +0700

    start read strings

--- LNG Translator/MainWindow.xaml.cs
+++ LNG Translator/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Forms;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 using System.Linq;
 
 namespace LNG_Translator
@@ -13,7 +15,7 @@ namespace LNG_Translator
     {
         private string windowTitle = "LNG Translator";
         private readonly OpenFileDialog OpenFileDialog = new OpenFileDialog();
-        private List<LNGRow> lngRows;
+        private List<LNGRow> lngRows = new List<LNGRow>();
 
         public MainWindow()
         {
@@ -42,17 +44,52 @@ namespace LNG_Translator
             {
                 byte[] signature = new byte[8];
                 fs.Read(signature, 0, signature.Length);
-                if (signature.Equals(knownSignature))
+                if (!signature.SequenceEqual(knownSignature))
                 {
                     System.Windows.MessageBox.Show("Unknown signature!");
                     return;
                 }
 
                 fs.Position = 0x40;
+                //byte[] addr = new byte[4] { 0x00, 0x00, 0xff, 0xff };
+                byte[] addr = new byte[4] { 0xff, 0xff, 0x00, 0x00 };
+                byte[] endaddr = new byte[4] { 0x00, 0x00, 0x00, 0x00 };
+                byte[] bChar = new byte[2] { 0xff, 0xff };
+                byte[] bEndChar = new byte[2] { 0x00, 0x00 };
+                List<byte> bText = new List<byte>();
+                int maxTextLength = 512;
+                while (!addr.SequenceEqual(endaddr))
+                {
+                    fs.Read(addr, 0, 2);
+                    long nextAddr = fs.Position;
+                    fs.Position = 0x40 + BitConverter.ToUInt32(addr, 0) * 2;
+                    fs.Read(bChar, 0, bChar.Length);
+                    while (maxTextLength > 0)
+                    {
+                        if (!bChar.SequenceEqual(bEndChar))
+                        {
+                            bText.Add(bChar[0]); bText.Add(bChar[1]);
+                            fs.Read(bChar, 0, bChar.Length);
+                            maxTextLength--;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    fs.Position = nextAddr;
+
+                    string sText = Encoding.Unicode.GetString(bText.ToArray());
+                    bText.Clear();
+                    lngRows.Add(new LNGRow(addr, sText));
+                }
+
+                stringsView.ItemsSource = lngRows;
+                stringsView.Items.Refresh();
             }
         }
 
-        private class LNGRow
+        public class LNGRow
         {
             private byte[] offset;
             private string origText;
